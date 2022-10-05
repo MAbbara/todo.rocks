47772007379589d4c927dbac678a495d8b53531f
MAbbara <mohammad@abbara.dev>
2022-10-05 10:59:55 +0300

    updated gitignore

--- viewList.php
+++ viewList.php
@@ -25,7 +25,7 @@
 ?>
 
 <!doctype html>
-<html class="dark">
+<html class="dark" lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
